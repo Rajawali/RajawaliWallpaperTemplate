169fcf19b01f0000c891e388e14fa291557eb49c
Dennis Ippel <ippeldv@gmail.com>
2013-05-15 18:21:36 +0100

    Updated the live wallpaper template for the texture-manager-rewrite branch.

--- src/com/mydomain/wallpaper/mywallpaper/Renderer.java
+++ src/com/mydomain/wallpaper/mywallpaper/Renderer.java
@@ -3,53 +3,55 @@ package com.mydomain.wallpaper.mywallpaper;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-import rajawali.animation.Animation3D;
+import rajawali.animation.Animation3D.RepeatMode;
 import rajawali.animation.RotateAnimation3D;
 import rajawali.lights.ALight;
 import rajawali.lights.DirectionalLight;
 import rajawali.materials.DiffuseMaterial;
+import rajawali.materials.textures.ATexture.TextureException;
+import rajawali.materials.textures.Texture;
 import rajawali.math.Number3D;
 import rajawali.primitives.Cube;
 import rajawali.renderer.RajawaliRenderer;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.view.animation.AccelerateDecelerateInterpolator;
 
 public class Renderer extends RajawaliRenderer {
-	private Animation3D mAnim;
-
 	public Renderer(Context context) {
 		super(context);
 	}
 
 	public void initScene() {
-		ALight light = new DirectionalLight();
-		light.setPower(1);
-		light.setPosition(0, 0, -5);
-		mCamera.setPosition(0, 0, -7);
-		mCamera.setLookAt(0, 0, 0);
+		ALight light = new DirectionalLight(-1, 0, -1);
+		light.setPower(2);
+		getCurrentCamera().setPosition(0, 0, 7);
+		getCurrentCamera().setLookAt(0, 0, 0);
 
-		Cube cube = new Cube(1);
-		DiffuseMaterial material = new DiffuseMaterial();
-		cube.setMaterial(material);
-		Bitmap texture = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.rajawali_tex);
-		cube.addTexture(mTextureManager.addTexture(texture));
-		cube.addLight(light);
-		addChild(cube);
+		try {
+			Cube cube = new Cube(1);
+			DiffuseMaterial material = new DiffuseMaterial();
+			material.addTexture(new Texture(R.drawable.rajawali_tex));
+			cube.setMaterial(material);
+			cube.addLight(light);
+			addChild(cube);
 
-		Number3D axis = new Number3D(3, 1, 6);
-		axis.normalize();
-		mAnim = new RotateAnimation3D(axis, 360);
-		mAnim.setDuration(8000);
-		mAnim.setRepeatCount(Animation3D.INFINITE);
-		mAnim.setInterpolator(new AccelerateDecelerateInterpolator());
-		mAnim.setTransformable3D(cube);
+			Number3D axis = new Number3D(3, 1, 6);
+			axis.normalize();
+			RotateAnimation3D anim = new RotateAnimation3D(axis, 360);
+			anim.setDuration(8000);
+			anim.setRepeatMode(RepeatMode.INFINITE);
+			anim.setInterpolator(new AccelerateDecelerateInterpolator());
+			anim.setTransformable3D(cube);
+			registerAnimation(anim);
+			anim.play();
+			
+		} catch (TextureException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 		super.onSurfaceCreated(gl, config);
-		mAnim.start();
 	}
 
 	public void onDrawFrame(GL10 glUnused) {
